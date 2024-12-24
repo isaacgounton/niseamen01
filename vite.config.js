1e287c111e4c791659b35f7d56284f39d34d9dde
Isaac Gounton <44446037+isaacgounton@users.noreply.github.com>
2024-12-24 00:49:24 -0500

    Manifest

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -10,29 +9,58 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,mp3}']
-      },
-      includeAssets: ['**/*'],
+      includeAssets: ['favicon.ico'],
       manifest: {
         name: 'NiseAmen',
         short_name: 'NiseAmen',
+        description: 'A collection of spiritual hymns of the Celestial Church of Christ.',
         theme_color: '#1e1b4b',
-        start_url: '/',
-        display: 'standalone',
         background_color: '#1e1b4b',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
         icons: [
           {
-            src: '/resources/AppImages/ios/192.png',
+            src: '/resources/AppImages/android/android-launchericon-192-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/resources/AppImages/ios/512.png',
+            src: '/resources/AppImages/android/android-launchericon-512-512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: '/resources/AppImages/android/android-launchericon-512-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,mp3}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module'
       }
     })
   ],
